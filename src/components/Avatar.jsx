27370e17567dfeafebf4b9baf1b74efd8d1766b7
Michael Nascimento <michael.nsc@outlook.com>
2023-03-27 16:53:31 -0300

    commit inicial

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,3 +1,15 @@
-export const Avatar = ({ src, alt }) => {
-  return <img src={src} className="img-thumbnail" alt={alt} />;
-};
+export class Avatar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <img
+        src={this.props.src}
+        className="img-thumbnail"
+        alt={this.props.alt}
+      />
+    );
+  }
+}
