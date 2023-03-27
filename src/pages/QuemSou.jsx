1062300277ee1e85050b701e2fd1e3f817a16a3e
Michael Nascimento <michael.nsc@outlook.com>
2023-03-27 15:35:14 -0300

    commit inicial

--- src/pages/QuemSou.jsx
+++ src/pages/QuemSou.jsx
@@ -5,15 +5,15 @@ import { Usuario } from "../components/Usuario";
 const redes = [
   {
     icone: "bi-linkedin",
-    link: "https://www.linkedin.com/in/michaelnsc/",
+    link: "https://www.linkedin.com/company/lab365/",
   },
   {
     icone: "bi-instagram text-danger",
-    link: "https://www.instagram.com/tiomikadev/",
+    link: "https://www.instagram.com/lab365_/",
   },
   {
     icone: "bi-github text-light-emphasis",
-    link: "https://github.com/mikansc",
+    link: "https://github.com/FullStack-Itaguacu",
   },
 ];
 
@@ -22,14 +22,13 @@ export const QuemSou = () => {
     <div className="">
       <div className="row border-bottom py-2">
         <div className="col-2">
-          <Avatar src="https://github.com/mikansc.png" alt="Michael" />
+          <Avatar
+            src="https://github.com/FullStack-Itaguacu.png"
+            alt="Lab365 - Full Stack Itaguaçu"
+          />
         </div>
         <div className="col ">
-          <Usuario
-            nome="Michael Nascimento"
-            cargo="Software Developer"
-            redes={redes}
-          />
+          <Usuario nome="Lab365" cargo="Instituição de ensino" redes={redes} />
         </div>
       </div>
       <div className="row mt-4">
