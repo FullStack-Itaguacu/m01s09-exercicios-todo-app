a0259aeba3510e950ac5ecd8059ce7f565d874d7
Michael Nascimento <michael.nsc@outlook.com>
2023-03-31 19:36:38 -0300

    pré-ajustes

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export class Bio extends React.Component {
   constructor(props) {
     super(props);
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -12,19 +12,19 @@ export const Formulario = ({ onSubmit }) => {
   return (
     <div className="mb-5 p-4 bg-secondary-subtle bg-gradient rounded">
       <h5>Adicionar tarefa</h5>
-      <form class="input-group mb-3" onSubmit={handleSubmit}>
-        <span class="input-group-text" id="campo-adicionar-tarefa">
+      <form className="input-group mb-3" onSubmit={handleSubmit}>
+        <span className="input-group-text" id="campo-adicionar-tarefa">
           Texto da tarefa:
         </span>
         <input
           ref={textElementRef}
           type="text"
-          class="form-control"
+          className="form-control"
           aria-label="Adicionar tarefa"
           aria-describedby="campo-adicionar-tarefa"
         />
         <button
-          class="btn btn-outline-primary bg-light"
+          className="btn btn-outline-primary bg-light"
           type="submit"
           id="button-addon2"
         >
