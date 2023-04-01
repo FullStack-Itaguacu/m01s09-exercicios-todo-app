0efc592bbaec90eb4df74e961547e9c45ee6d399
Michael Nascimento <michael.nsc@outlook.com>
2023-03-31 22:03:47 -0300

    exercicio 6 ok

--- src/contexts/ToDoProvider.jsx
+++ src/contexts/ToDoProvider.jsx
@@ -1,10 +1,46 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const todoContext = createContext();
 
 export const ToDoProvider = (props) => {
+  const [toDos, setToDos] = useState([
+    { id: 1, text: "Estudar React", done: false },
+    { id: 2, text: "Praticar com um projeto", done: true },
+    { id: 3, text: "Divulgar meu resultado no LinkedIn", done: false },
+  ]);
+
+  const addToDo = (text) => {
+    const newToDo = {
+      id: Date.now(),
+      text,
+      done: false,
+    };
+    setToDos([...toDos, newToDo]);
+  };
+
+  const markTodo = (id) => {
+    const newToDos = toDos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, done: !todo.done };
+      }
+      return todo;
+    });
+    setToDos(newToDos);
+  };
+
+  const completedToDos = toDos.filter((todo) => todo.done);
+  const pendingToDos = toDos.filter((todo) => !todo.done);
+
+  const contextValues = {
+    toDos,
+    addToDo,
+    markTodo,
+    completedToDos,
+    pendingToDos,
+  };
+
   return (
-    <todoContext.Provider value={"meu contexto"}>
+    <todoContext.Provider value={contextValues}>
       {props.children}
     </todoContext.Provider>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,34 +4,6 @@ import { Formulario } from "../components/Formulario";
 import { ToDoProvider } from "../contexts/ToDoProvider";
 
 export const Home = () => {
-  const [toDos, setToDos] = useState([
-    { id: 1, text: "Estudar React", done: false },
-    { id: 2, text: "Praticar com um projeto", done: true },
-    { id: 3, text: "Divulgar meu resultado no LinkedIn", done: false },
-  ]);
-
-  const addToDo = (text) => {
-    const newToDo = {
-      id: Date.now(),
-      text,
-      done: false,
-    };
-    setToDos([...toDos, newToDo]);
-  };
-
-  const markTodo = (id) => {
-    const newToDos = toDos.map((todo) => {
-      if (todo.id === id) {
-        return { ...todo, done: !todo.done };
-      }
-      return todo;
-    });
-    setToDos(newToDos);
-  };
-
-  const completedToDos = toDos.filter((todo) => todo.done);
-  const pendingToDos = toDos.filter((todo) => !todo.done);
-
   return (
     <ToDoProvider>
       <div className="row">
